72bee09beee9dff8cb98a21394eff0b1ba1142e7
RemyA94 <remyjoseacostamelo@gmail.com>
2022-02-10 23:28:24 -0400

    Add a TipoCuentasController Add a TipoCuenta DTO Add a View Crear

---
+++ BudgetManagement/Controllers/TiposCuentasController.cs
@@ -0,0 +1,23 @@
+﻿using BudgetManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManagement.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }  
+        
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta )
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View(tipoCuenta);
+            }
+            return View();
+        }
+    }
+}
---
+++ BudgetManagement/Models/TipoCuenta.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetManagement.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es requerido.")]
+        [StringLength(maximumLength: 50, MinimumLength = 2, ErrorMessage ="La longitud del campo {0} debe de estar entre {1} y {2}")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
