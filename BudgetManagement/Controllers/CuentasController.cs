a83c02fb22a7bd0bf30b1984483ba281db6dedc1
RemyA94 <remyjoseacostamelo@gmail.com>
2022-03-10 23:55:53 -0400

    Update Crear Cuentas

--- BudgetManagement/Controllers/CuentasController.cs
+++ BudgetManagement/Controllers/CuentasController.cs
@@ -9,23 +9,50 @@ namespace BudgetManagement.Controllers
     {
         private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
         private readonly IServiciosUsuarios serviciosUsuarios;
+        private readonly IRepositorioCuentas repositorioCuentas;
 
         public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,
-            IServiciosUsuarios serviciosUsuarios)
+            IServiciosUsuarios serviciosUsuarios, IRepositorioCuentas repositorioCuentas)
         {
             this.repositorioTiposCuentas = repositorioTiposCuentas;
             this.serviciosUsuarios = serviciosUsuarios;
+            this.repositorioCuentas = repositorioCuentas;
         }
 
         [HttpGet]
         public async Task<IActionResult> Crear()
         {
             var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
-            var tipoCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
             var modelo = new CuentaCreacionViewModel(); 
 
-            modelo.TiposCuentas = tipoCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+            modelo.TiposCuentas = await ObtenerTiposCuentas(usuarioId);
             return View(modelo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(CuentaCreacionViewModel cuenta)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tipoCuentas = await repositorioTiposCuentas.ObtenerPorId(cuenta.TipoCuentasId, usuarioId);
+
+            if (tipoCuentas is null) 
+            {
+                return RedirectToAction("NO Encontrado", "Home");
+            }
+            if (!ModelState.IsValid) 
+            {
+                cuenta.TiposCuentas = await ObtenerTiposCuentas(usuarioId);
+                return View(cuenta);
+            }
+            await repositorioCuentas.Crear(cuenta);
+            return RedirectToAction("Index");
+
+
+        }
+        private async Task<IEnumerable<SelectListItem>> ObtenerTiposCuentas(int usuarioId) 
+        {
+            var tipoCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            return tipoCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
         }
     }
 }
---
+++ BudgetManagement/Servicios/RepositorioCuentas.cs
@@ -0,0 +1,31 @@
+﻿using BudgetManagement.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BudgetManagement.Servicios
+{
+    public interface IRepositorioCuentas
+    {
+        Task Crear(Cuenta cuenta);
+    }
+    public class RepositorioCuentas : IRepositorioCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Cuenta cuenta) 
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(
+                @"Insert Into Cuentas (Nombre, TipoCuentaId, Descripcion, Balance)
+                 Values (@Nombre, @TipoCuentaId, @Descripcion, @Balance);
+                 Select SCOPE_IDENTITY();", cuenta);
+
+            cuenta.Id = id;
+            
+        }
+    }
+}
